c9fdfabadc1ba14d82d25bc6c635b9c5e62e324f
galaxy119 <amathor929@gmail.com>
2019-03-08 06:28:33 -0800

    Initial Commit

---
+++ MVP/MVP/EventsHandler.cs
@@ -0,0 +1,72 @@
+using Smod2;
+using Smod2.Events;
+using Smod2.EventSystem;
+using Smod2.EventHandlers;
+using Smod2.API;
+using System.Collections.Generic;
+using scp4aiur;
+
+namespace MVP
+{
+	internal class EventsHandler : IEventHandlerPlayerDie, IEventHandlerPlayerJoin, IEventHandlerWaitingForPlayers, IEventHandlerRoundStart, IEventHandlerRoundEnd
+	{
+		private readonly MVP plugin;
+		public EventsHandler(MVP plugin) => this.plugin = plugin;
+
+		public void OnWaitingForPlayers(WaitingForPlayersEvent ev)
+		{
+			MVP.enabled = this.plugin.GetConfigBool("mvp_enable");
+			MVP.count_grenades = this.plugin.GetConfigBool("mvp_count_grenades");
+			MVP.track_scp_kills = this.plugin.GetConfigBool("mvp_track_scp_kills");
+			MVP.track_scps = this.plugin.GetConfigBool("mvp_track_scps");
+			MVP.multi_kill = this.plugin.GetConfigBool("mvp_multikill");
+			MVP.multi_kill_delay = this.plugin.GetConfigFloat("mvp_multikill_delay");
+			MVP.multi_kill_num = this.plugin.GetConfigInt("mvp_multikill_num");
+			MVP.multi_text = this.plugin.GetConfigString("mvp_multi_text");
+		}
+		public void OnRoundStart(RoundStartEvent ev)
+		{
+			foreach (Player player in plugin.Server.GetPlayers())
+			{
+				MVP.killCounter[player.Name] = 0;
+				MVP.multi_track[player.Name] = false;
+				MVP.multikill[player.Name] = 0;
+			}
+		}
+		public void OnPlayerJoin(PlayerJoinEvent ev)
+		{
+			MVP.killCounter[ev.Player.Name] = 0;
+			MVP.multi_track[ev.Player.Name] = false;
+			MVP.multikill[ev.Player.Name] = 0;
+		}
+		public void OnPlayerDie(PlayerDeathEvent ev)
+		{
+			if (MVP.enabled)
+			{
+				if (MVP.track_scps && ev.Killer.TeamRole.Team == Smod2.API.Team.SCP)
+				{
+					MVP.killCounter[ev.Killer.Name]++;
+					MVP.multikill[ev.Killer.Name]++;
+				}
+				else if (MVP.track_scp_kills && ev.Player.TeamRole.Team == Smod2.API.Team.SCP)
+				{
+					MVP.killCounter[ev.Killer.Name]++;
+					MVP.multikill[ev.Killer.Name]++;
+				}
+				else if (ev.Player.TeamRole.Team != Smod2.API.Team.SCP && ev.Killer.TeamRole.Team != Smod2.API.Team.SCP)
+				{
+					MVP.killCounter[ev.Killer.Name]++;
+					MVP.multikill[ev.Killer.Name]++;
+				}
+				if (MVP.multi_kill)
+				{
+					Timing.Run(Functions.singleton.MultiKill(ev.Killer, MVP.multi_kill_delay));
+				}
+			}
+		}
+		public void OnRoundEnd(RoundEndEvent ev)
+		{
+			Functions.singleton.Announce();
+		}
+	}
+}
---
+++ MVP/MVP/MVPFunctions.cs
@@ -0,0 +1,71 @@
+using Smod2;
+using Smod2.API;
+using System;
+using Smod2.Events;
+using System.Collections.Generic;
+using System.Linq;
+using Smod2.Commands;
+
+namespace MVP
+{
+	public class Functions
+	{
+		public static Functions singleton;
+		public MVP MVP;
+		public Functions(MVP plugin)
+		{
+			this.MVP = plugin;
+			Functions.singleton = this;
+		}
+
+		public IEnumerable<float> MultiKill(Player player, float delay)
+		{
+			if (!MVP.multi_track[player.Name])
+			{
+				MVP.multi_track[player.Name] = true;
+				yield return delay;
+				if (MVP.multikill[player.Name] >= MVP.multi_kill_num)
+				{
+					MVP.Server.Map.ClearBroadcasts();
+					MVP.Server.Map.Broadcast(10, player.Name + " " + MVP.multi_text, false);
+				}
+				MVP.multi_track[player.Name] = false;
+				MVP.multikill[player.Name] = 0;
+			}
+		}
+		public void Announce()
+		{
+			if (MVP.enabled)
+			{
+				KeyValuePair<string, int> max = new KeyValuePair<string, int>();
+				foreach (var kvp in MVP.killCounter)
+				{
+					if (kvp.Value > max.Value)
+						max = kvp;
+				}
+				MVP.Server.Map.ClearBroadcasts();
+				MVP.Server.Map.Broadcast(25, "MVP: " + max.Key + " killed the most players, with " + max.Value + " total kills!", false);
+			}
+		}
+		public void Enable()
+		{
+			MVP.enabled = true;
+			MVP.Info("Refreshing player list..");
+			foreach (Player player in MVP.Server.GetPlayers())
+			{
+				if (!MVP.killCounter.ContainsKey(player.Name))
+				{
+					MVP.killCounter[player.Name] = 0;
+				}
+				if (!MVP.multi_track.ContainsKey(player.Name))
+				{
+					MVP.multi_track[player.Name] = false;
+				}
+				if (!MVP.multikill.ContainsKey(player.Name))
+				{
+					MVP.multikill[player.Name] = 0;
+				}
+			}
+		}
+	}
+}
